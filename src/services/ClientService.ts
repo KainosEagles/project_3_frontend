90ac011178409703580d6fdeaf430de7d9a7b957
JakubW-k <jakub.wiecek@kainos.com>
2024-08-15 13:25:09 +0200

    display all clients feature

--- src/services/ClientService.ts
+++ src/services/ClientService.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 import { TopClientResponse } from "../models/TopClientResponse";
+import { ClientRequest } from "http";
 
 export const getClientTop = async (): Promise<TopClientResponse> => {
     try{
@@ -9,6 +10,16 @@ export const getClientTop = async (): Promise<TopClientResponse> => {
     } catch (e) {
         console.log(e);
         throw new Error('Failed to get client with highest value of projects');
+    }
+}
+
+export const getAllClients = async (): Promise<ClientRequest[]> => {
+    try {
+        const response: AxiosResponse = await axios.get("http://localhost:8080/api/clients");
+        return response.data;
+    } catch (e) {
+        console.log(e);
+        throw new Error("Failed to load clients");
     }
 }
 
