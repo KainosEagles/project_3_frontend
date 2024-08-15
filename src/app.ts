0f86cf9cbcbfab60a31200d060a47499c4ca22f8
amordzon <97406794+amordzon@users.noreply.github.com>
2024-08-15 11:57:04 +0200

    feature: create project (#4)
    * create project
    
    * linter error fixes
    
    * linter fix
    
    ---------
    
    Co-authored-by: “Aleksandra” <“aleksandramordzon@gmail.com”>

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import nunjucks from "nunjucks";
 import bodyParser from "body-parser";
 import session from "express-session";
 
+import { getProjectForm, postProjectForm } from "./controllers/ProjectController";
 import { deliveryEmployees, salesEmployees } from "./controllers/EmployeeController";
 
 const app = express();
@@ -29,5 +30,7 @@ app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
+app.get('/projectForm', getProjectForm);
+app.post('/projectForm', postProjectForm);
 app.get('/deliveryEmployees', deliveryEmployees);
 app.get('/salesEmployees', salesEmployees);
