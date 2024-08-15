31d07a9c433a4938836afbf6031fb74ff7dcd998
JakubW-k <jakub.wiecek@kainos.com>
2024-08-15 16:57:19 +0200

    menu

--- src/app.ts
+++ src/app.ts
@@ -30,6 +30,11 @@ app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
+app.get('/', async (req: express.Request, res: express.Response) => {
+  
+  res.render('index.html');
+});
+
 app.get('/clientTop', getClientWithHighestValueOfProjects);
 app.get('/clientsWithDetails', getClientsWithDetails);
 
