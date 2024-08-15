fbed39b86b7db0f3b63d4754efcf6d1ddd8f7d0c
DariuszMech <dariuszmech@10g.pl>
2024-08-15 13:58:47 +0200

    linter fix

--- src/controllers/ClientController.ts
+++ src/controllers/ClientController.ts
@@ -6,6 +6,7 @@ export const getClientWithHighestValueOfProjects = async (req: express.Request, 
 
 export const getClientsWithDetails = async (req: express.Request, res: express.Response): Promise<void> => {
     res.render('clientWithDetils.html', { clients: await getAllClientsWithDetails() });
+}
   
 export const allClients = async (req: express.Request, res: express.Response): Promise<void> => {
     res.render('clientList.html', {clients: await getAllClients()});
